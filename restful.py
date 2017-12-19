059c729b499569cd31abbaa3c7b0fea91f77bde6
James Dindi <dindijjames@gmail.com>
2017-12-19 20:47:27 +0300

    [part 3]: PUT requests

--- restful.py
+++ restful.py
@@ -32,5 +32,11 @@ def addOne():
     languages.append(language)
     return jsonify({'languages' : languages})
 
+@app.route('/lang/<string:name>', methods=['PUT'])
+def editOne(name):
+    langs = [language for language in languages if language['name'] == name]
+    langs[0]['name'] = request.json['name']
+    return jsonify({'language' : langs[0]})
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080) # run app inport 8080 in debug mode
