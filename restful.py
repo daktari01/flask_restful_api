951477151fe057d0702bc2d7877ff8b557977a01
James Dindi <dindijjames@gmail.com>
2017-12-19 20:57:11 +0300

    [part 4]: DELETE requests

--- restful.py
+++ restful.py
@@ -38,5 +38,11 @@ def editOne(name):
     langs[0]['name'] = request.json['name']
     return jsonify({'language' : langs[0]})
 
+@app.route('/lang/<string:name>', methods=['DELETE'])
+def removeOne(name):
+    lang = [language for language in languages if language['name'] == name]
+    languages.remove(lang[0])
+    return jsonify({'languages' : languages})
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080) # run app inport 8080 in debug mode
